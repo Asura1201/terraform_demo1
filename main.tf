baf0465b04b0e9650a848f5451483cb3a729de91
Asura1201 <asurainferno1000@gmail.com>
2024-12-20 07:20:49 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,6 +12,18 @@ provider "azurerm" {
 }
 
 resource "azurerm_resource_group" "mydemorg" {
-  name     = "demorg"
-  location = "West Europe"
+  name     =  var.resource_group_name
+  location =  var.location
+}
+
+module "keyvault" {
+  source                    =  "./modules/keyvault"
+  keyvault_name             =  var.keyvault_name
+  location                  =  var.location
+  resource_group_name       =  var.resource_group_name
+  tenant_id                 =  var.tenant_id
+  sku_keyvault              =  var.sku_keyvault
+  purge_protection_enabled  =  var.purge_protection_enabled
+  object_id                 =  var.object_id
+  retention_period          =  var.retention_period
 }
