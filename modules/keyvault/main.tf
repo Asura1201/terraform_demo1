305c01fdde0cfa30eed094c1c553baff733ee0ee
Asura1201 <asurainferno1000@gmail.com>
2024-12-20 07:01:37 +0530

    Create main.tf

---
+++ modules/keyvault/main.tf
@@ -0,0 +1,31 @@
+resource "azurerm_key_vault" "example" {
+  name                        = var.keyvault_name
+  location                    = var.location
+  resource_group_name         = var.resource_group_name
+  enabled_for_disk_encryption = true
+  tenant_id                   = var.tenant_id
+  soft_delete_retention_days  = var.retention_period
+  purge_protection_enabled    = var.purge_protection_enabled
+
+  sku_name = "standard"
+
+  access_policy {
+    tenant_id = var.tenant_id
+    object_id = var.object_id
+
+    key_permissions = [
+      "Get",
+      "Set"
+    ]
+
+    secret_permissions = [
+      "Get",
+      "Set"
+    ]
+
+    storage_permissions = [
+      "Get",
+      "Set"
+    ]
+  }
+}
